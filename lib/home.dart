79322238ff7d2f630f707210f83f8813bb71a521
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-03 16:38:08 +0700

    update

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'web_view_container.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-
 class Home extends StatelessWidget {
-  // final _links = ['http://localhost:3000/auth'];
-	// final Completer<WebViewController> _controller = Completer<WebViewController>();
 	var _url = 'http://localhost:3000/auth';
   final _key = UniqueKey();
 
@@ -28,26 +24,5 @@ class Home extends StatelessWidget {
           ],
       )
     );
-  }
-
-  Widget _urlButton(BuildContext context, String url) {
-    return Container(
-      padding: EdgeInsets.all(20.0),
-      child: FlatButton(
-        color: Theme.of(context).primaryColor,
-        padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
-        child: Text(url),
-        onPressed: () => _handleURLButtonPress(context, url),
-      )
-		);
-  }
-
-  void _handleURLButtonPress(BuildContext context, String url) {
-    Navigator.push(
-			context,
-			MaterialPageRoute(
-				builder: (context) => WebViewContainer(url)
-			)
-			);
   }
 }
